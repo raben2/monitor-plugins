cd5189053d20afb76f8c5b2fa0e77245bfd75e9c
Arindam Choudhury <arindam@live.com>
2015-08-04 15:52:45 +0200

    run command

--- mplugins/run_cmd/run_cmd.py
+++ mplugins/run_cmd/run_cmd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 import shlex
 import subprocess
@@ -6,43 +7,47 @@ import subprocess
 sys.path.append('../../../plugins')
 
 from __mplugin import MPlugin
-from __mplugin import OK, CRITICAL, TIMEOUT
+from __mplugin import OK, CRITICAL
+
 
 class RunCmd(MPlugin):
     def run(self):
         cmd = self.config.get('cmd')
 
-        # Verify URL
-        if not url:
-            self.exit(CRITICAL, message="no commands found")
+        result = self._run_command(cmd)
+        data = {}
 
+        if not result:
+            self.exit(CRITICAL, message="Empty output from the command")
 
-        data = self._run_command(cmd)
+        if isinstance(data, str):
+            data['result_str'] = data
+            data['result_num'] = None
+        elif isinstance(data, int) or isinstance(data, float):
+            data['result_num'] = data
+            data['result_str'] = None
+        else:
+            self.exit(CRITICAL, message="Unsupported opera")
         metrics = {
-            'Connections': {
-                'accepted conn': self._counter(data['accepted conn'],'accepted conn')
-            },
-            'Processes': {
-               'idle processes': data['idle processes'],
-               'active processes': data['active processes'],
-               'total processes': data['total processes']
+            'result': {
+                'result_str': data['result_str'],
+                'result_num':data['result_num']
+
+                }
             }
-        }
-        
-        self.exit(OK,data,metrics)
-        
+        self.exit(OK, data, metrics)
+
     def _run_command(self, cmd):
         commands = cmd.split('|')
-        res = ''
+        res = None
         for command in commands:
             try:
-                p = subprocess.Popen(shlex.split(command),stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+                p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
                 if res:
                     p.stdin.write(res)
                 res = p.communicate()[0]
             except OSError:
-                print "can not run the command"
-                res = ''
+                self.exit(CRITICAL, message="Unable to run command")
         return res
 
     
