0bc713fcb406be25524cfa3e421f98289a9f6f35
Juan Carlos Moreno <juancarlos.moreno@ecmanaged.com>
2015-08-25 16:41:29 +0200

    run_cmd changes

--- mplugins/run_cmd/run_cmd.py
+++ mplugins/run_cmd/run_cmd.py
@@ -12,18 +12,18 @@ from __mplugin import OK, CRITICAL
 
 class RunCmd(MPlugin):
     def run(self):
-        cmd = self.config.get('cmd')
+        cmd = self.config.get('command')
+        
+        if not cmd:
+            self.exit(CRITICAL, message="Invalid command")
         
         result, stdout = self._run_command(cmd)
         if result: result = CRITICAL
 
-        data = metrics = {}
-        if self._is_string(stdout):
-            data['result_str'] = data
-            data['result_num'] = None
-
-        elif self._is_number(stdout):
-            data['result_num'] = data
+        data = {}
+        metrics = {}
+        if self._is_number(stdout):
+            data['result_num'] = stdout
             data['result_str'] = None
             
             # Metric only if is numeric
@@ -33,6 +33,10 @@ class RunCmd(MPlugin):
               }
             }    
 
+        elif self._is_string(stdout):
+            data['result_str'] = stdout
+            data['result_num'] = None
+
         else:
             self.exit(CRITICAL, message="Unsupported result")
 
@@ -41,7 +45,8 @@ class RunCmd(MPlugin):
     def _run_command(self, cmd):
         commands = cmd.split('|')
 
-        result = stdout = None
+        result = 2
+        stdout = ''
         for command in commands:
             try:
                 p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -53,7 +58,19 @@ class RunCmd(MPlugin):
 
             except OSError:
                 self.exit(CRITICAL, message="Unable to run command")
-                
+
+        # Try to conver string to int / float
+        stdout = stdout.rstrip('\n')
+        try:
+            if str(float(stdout)) == stdout:
+                stdout = float(stdout)
+        except: pass
+        
+        try:
+            if str(int(stdout)) == stdout:
+                stdout = int(stdout)
+        except: pass
+             
         return result, stdout
     
 monitor = RunCmd()
