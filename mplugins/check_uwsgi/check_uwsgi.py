66a868c6414e40827f6d717358d32daa1d1d24fe
Arindam Choudhury <arindam@live.com>
2015-10-26 16:23:55 +0100

    which moved to __mplugin

--- mplugins/check_uwsgi/check_uwsgi.py
+++ mplugins/check_uwsgi/check_uwsgi.py
@@ -9,26 +9,6 @@ from __mplugin import OK, CRITICAL, TIMEOUT
 
 class UWSGIStatus(MPlugin):
 
-    def which(program):
-        import os
-        def is_exe(fpath):
-            return (os.path.exists(fpath) and
-                    os.access(fpath, os.X_OK) and
-                    os.path.isfile(os.path.realpath(fpath)))
-
-        fpath, fname = os.path.split(program)
-        if fpath:
-            if is_exe(program):
-                return program
-        else:
-            if "PATH" not in os.environ:
-                return None
-            for path in os.environ["PATH"].split(os.pathsep):
-                exe_file = os.path.join(path, program)
-                if is_exe(exe_file):
-                    return exe_file
-        return None
-
     def get_stats(self):
         from commands import getstatusoutput
         import json
